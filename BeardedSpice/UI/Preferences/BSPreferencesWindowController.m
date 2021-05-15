5038c70ff6ac1b6fa2b64b520349c34511bb4423
Roman Sokolov <still2.mail@me.com>
2021-05-15 14:26:35 +0300

    skipci: some fixes

--- BeardedSpice/UI/Preferences/BSPreferencesWindowController.m
+++ BeardedSpice/UI/Preferences/BSPreferencesWindowController.m
@@ -16,8 +16,7 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
-    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    [self.window center];
 }
 
 // We redefine this method. Now method does nothing.
