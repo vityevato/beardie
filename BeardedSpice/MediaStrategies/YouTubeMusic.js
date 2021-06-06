544a41dfe9f03772b72662019edc3a069f95c68c
Roman Sokolov <still2.mail@me.com>
2021-06-06 19:18:30 +0300

    Fixed Youtube Music strategy

--- BeardedSpice/MediaStrategies/YouTubeMusic.js
+++ BeardedSpice/MediaStrategies/YouTubeMusic.js
@@ -3,11 +3,12 @@
 //  BeardedSpice
 //
 //  Created by Vladislav Gapurov on 07/28/18
+//  Modified by Roman Sokolov on 06/06/21
 //  Copyright (c) 2021 GPL v3 http://www.gnu.org/licenses/gpl.html
 //
 
 BSStrategy = {
-  version: 2,
+  version: 3,
   displayName: "YouTube Music",
   homepage: "https://music.youtube.com/",
   accepts: {
@@ -25,14 +26,14 @@ BSStrategy = {
     }
   },
   favorite: function() {
-    document.querySelector('ytmusic-like-button-renderer .ytmusic-like-button-renderer.like').click()
+    document.querySelector('ytmusic-player-bar ytmusic-like-button-renderer .ytmusic-like-button-renderer.like').click()
   },
   trackInfo: function() {
     let timeInfo = document.querySelector('.ytmusic-player-bar.time-info').innerHTML.split('/');
     let thumb = document.querySelector('.ytmusic-player-bar img');
     let title = document.querySelector('.ytmusic-player-bar.title');
     let byline = document.querySelector('.byline.ytmusic-player-bar');
-    let like = document.querySelector('ytmusic-like-button-renderer');
+    let like = document.querySelector('ytmusic-player-bar ytmusic-like-button-renderer');
 
     return {
       'image': thumb.src,
