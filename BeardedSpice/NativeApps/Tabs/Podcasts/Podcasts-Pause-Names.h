a1245c9d38733ad0632a01838fba4603593031be
Roman Sokolov <still2.mail@me.com>
2021-08-04 22:59:04 +0300

    skipci: #19 Getting "pause" localizations

---
+++ BeardedSpice/NativeApps/Tabs/Podcasts/Podcasts-Pause-Names.h
@@ -0,0 +1,41 @@
+#define PODCASTS_PAUSE_NAMES @[\
+@"إيقاف مؤقت",\
+@"Pausa",\
+@"Pozastavit",\
+@"Pause",\
+@"Anhalten",\
+@"Παύση",\
+@"Pause",\
+@"Pause",\
+@"Pause",\
+@"Pausa",\
+@"Pausa",\
+@"Keskeytä",\
+@"Pause",\
+@"Pause",\
+@"השהה",\
+@"पॉज़ करें",\
+@"Pauziraj",\
+@"Szünet",\
+@"Jeda",\
+@"Pausa",\
+@"一時停止",\
+@"일시 정지",\
+@"Jeda",\
+@"Pauzeer",\
+@"Pause",\
+@"Wstrzymaj",\
+@"Pausar",\
+@"Pausar",\
+@"Suspendă",\
+@"Приостановить",\
+@"Pozastaviť",\
+@"Pausa",\
+@"หยุดพัก",\
+@"Duraklat",\
+@"Пауза",\
+@"Tạm dừng",\
+@"暂停",\
+@"暫停",\
+@"暫停",\
+]
