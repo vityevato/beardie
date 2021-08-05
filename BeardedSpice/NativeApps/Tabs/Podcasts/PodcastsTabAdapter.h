ff84a9c59857e7b1df9c9bbcadbf27481d925c47
Roman Sokolov <still2.mail@me.com>
2021-08-05 05:26:23 +0300

    Added supporting Apple Podcasts app

---
+++ BeardedSpice/NativeApps/Tabs/Podcasts/PodcastsTabAdapter.h
@@ -0,0 +1,17 @@
+//
+//  PodcastsTabAdapter.h
+//  Beardie
+//
+//  Created by Roman Sokolov on 05.08.2021.
+//  Copyright © 2021 GPL v3 http://www.gnu.org/licenses/gpl.html
+//
+
+#import "BSNativeAppTabAdapter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PodcastsTabAdapter : BSNativeAppTabAdapter
+
+@end
+
+NS_ASSUME_NONNULL_END
