562d88be73e649ab3ca88a4d368e90b845fd1bf4
Roman Sokolov <still2.mail@me.com>
2022-09-06 18:17:01 +0300

    skipci: Updated MixCloud adapter

--- BeardedSpice/MediaStrategies/MixCloud.js
+++ BeardedSpice/MediaStrategies/MixCloud.js
@@ -1,27 +1,33 @@
 //
-//  MixCloud.plist
+//  MixCloud
 //  BeardedSpice
 //
-//  Created by Tyler Rhodes on 2/23/14.
-//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
+//  Created by Roman Sokolov.
+//  Copyright (c) 2015-2020 GPL v3 http://www.gnu.org/licenses/gpl.html
 //
 BSStrategy = {
-  version:4,
+  version:6,
   displayName:"Mixcloud",
-  homepage: "https://www.mixcloud.com",
   accepts: {
     method: "predicateOnTab",
     format:"%K LIKE[c] '*www.mixcloud.com*'",
     args: ["URL"]
   },
-  isPlaying: function() {return (document.querySelector('.player-control.pause-state') != null)},
-  pause: function () { var aButton = document.querySelector('.player-control.pause-state'); if(aButton) aButton.click() },
-  toggle: function () { document.querySelector('.player-control').click(); },
+  isPlaying: function() {return (BSStrategy.pauseButton() != null)},
+  pause: function () { BSStrategy.pauseButton().click() },
+  toggle: function () { ( BSStrategy.pauseButton() || BSStrategy.playButton()).click(); },
+  next: function() {document.querySelector('button[aria-label="Seek forwards"]').click()},
+  previous: function() {document.querySelector('button[aria-label="Seek backwards"]').click()},
+  favorite: function() {document.querySelector('span[class*="PlayerActionsFavoriteButton__PlayerFavoriteIcon-"]').click()},
   trackInfo: function () {
     return {
-      'track': document.querySelector('.player-cloudcast-title').text,
-      'artist': document.querySelector('.player-cloudcast-author-link').text,
-      'image' : document.querySelector('.player .player-cloudcast-image img').getAttribute('src')
-    }
-  }
+      'track': document.querySelector('p[class*="PlayerControls__ShowTitle-"]').textContent,
+      'artist': document.querySelector('span[class*="PlayerControls__ShowOwnerName-"]').textContent,
+      'image' : document.querySelector('div[class*="PlayerControls__ShowPicture-"]>img').currentSrc,
+      'progress': document.querySelector('div[class*="PlayerSliderComponent__StartTime-"]').textContent + '( remains ' + document.querySelector('div[class*="PlayerSliderComponent__EndTime-"]').textContent + ')',
+      'favorited': (document.querySelector('span[class*="PlayerActionsFavoriteButton__PlayerFavoriteIcon-"]').getAttribute('data-tooltip') == "Undo Favorite")
+    };
+  },
+  pauseButton: function() {return document.querySelector('div[aria-label="Pause"]') || document.querySelector('.shaka-play-button[icon="pause"]'); },
+  playButton: function() {return document.querySelector('div[aria-label="Play"]') || document.querySelector('.shaka-play-button[icon="play"]'); }
 }
